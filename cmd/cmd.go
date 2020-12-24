db01b20f6d9a95674f1f2fc63ef8117d3ab209bf
takutaka <takutaka@pepabo.com>
2020-12-24 13:15:03 +0900

    create map

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/kjk/notionapi"
-	"github.com/kjk/notionapi/tomarkdown"
 	"github.com/urfave/cli"
 )
 
@@ -26,6 +26,13 @@ func main() {
 	app.Run(os.Args)
 }
 
+type Body struct {
+	Content   []byte
+	Title     string
+	Tags      []string
+	UpdatedAt time.Time
+}
+
 func action(c *cli.Context) error {
 	client := &notionapi.Client{
 		AuthToken: os.Getenv("NOTION_TOKEN"),
@@ -35,10 +42,17 @@ func action(c *cli.Context) error {
 	if err != nil {
 		log.Fatalf("DownloadPage() failed with %s\n", err)
 	}
-	fmt.Println(page.TableViews[0].Rows[0].Page.GetTitle()[0].Text)
-	for _, id := range page.Root().ContentIDs {
-		fmt.Println(id)
+	tb := page.TableViews[0]
+	permMap := map[string]time.Time{}
+	for _, row := range tb.Rows {
+		permMap[row.Columns[1][0].Text] = row.Page.LastEditedOn()
+		fmt.Println(row.Page.LastEditedOn())
+		// p, err := client.DownloadPage(row.Page.ID)
+		// if err != nil {
+		// 	return err
+		// }
+		// fmt.Printf("%s", tomarkdown.ToMarkdown(p))
 	}
-	fmt.Printf("%s", tomarkdown.ToMarkdown(page))
+	fmt.Printf("%s", permMap)
 	return nil
 }
